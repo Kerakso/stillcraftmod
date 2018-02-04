8fe1330853a07866c5c0ba3d4f990638dd7a564f
Kerakso <20502639+Kerakso@users.noreply.github.com>
2018-02-04 19:04:51 +0100

    :E

---
+++ stillcraft/common/defines/modded_defines.lua
@@ -0,0 +1,8 @@
+NDefines.NTechnology.BASE_RESEARCH_POINTS_SAVED = 100.0		-- Base amount of research points a country can save per slot
+NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 70				-- This much progress can be saved while not having a focus selected
+
+NDefines.NAI.UPGRADE_XP_RUSH_UPDATE = 400				-- If XP is above this on the daily tick the AI will attempt to spend it
+
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 999			--Max army experience a country can store
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 999			--Max navy experience a country can store
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 999				--Max air experience a country can store
